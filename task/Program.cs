8a09e6c81bce303d49a1102270e0165082810015
Voyagerr_77 <whydowefall91@gmail.com>
2024-03-16 17:44:15 +0200

    program

---
+++ task/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+Console.WriteLine("Введите элементы массива через запятую:");
+string input = Console.ReadLine();
+string[] inputArray = input.Split(',');
+
+
+string[] resultArray = FilterStrings(inputArray);
+
+Console.WriteLine("Результат:");
+foreach (string item in resultArray)
+{
+    Console.Write(item + ", ");
+}
+
+Console.ReadLine();
+
+
+string[] FilterStrings(string[] inputArray)
+{
+    int count = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+
+    int index = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[index] = inputArray[i];
+            index++;
+        }
+    }
+
+    return resultArray;
+}
